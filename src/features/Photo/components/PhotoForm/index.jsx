04f06187931d9aae590d46b36b10e9d3d001df73
Duy <trankhanhduy8599@gmail.com>
2020-06-19 20:09:17 +0700

    Change direction using jsconfig

--- src/features/Photo/components/PhotoForm/index.jsx
+++ src/features/Photo/components/PhotoForm/index.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 import Select from 'react-select'
-import Images from '../../../../constants/images';
-import { PHOTO_CATEGORY_OPTIONS } from '../../../../constants/global';
+import { PHOTO_CATEGORY_OPTIONS } from 'constants/global';
+import Images from 'constants/images';
 
 PhotoForm.propTypes = {
   onSubmit: PropTypes.func,
--- src/features/Photo/pages/AddEdit/index.jsx
+++ src/features/Photo/pages/AddEdit/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Banner from '../../../../components/Banner';
-import PhotoForm from '../../components/PhotoForm';
 import './styles.scss';
+import Banner from 'components/Banner';
+import PhotoForm from 'features/Photo/components/PhotoForm';
 
 AddEditPage.propTypes = {};
 
